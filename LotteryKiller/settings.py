e6b88d3be7e7ba0acc9a032507dccf15bfc7c6a6
bonesignited <zlxexit@gmail.com>
2018-08-06 17:05:10 +0800

    before deployment

--- LotteryKiller/settings.py
+++ LotteryKiller/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '#y_^3)u_a%15q_$*j#e5fh0&#@n7sh+ptt+2=#%xe18q_=278$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', '39.107.254.255']
 
 
 # Application definition
@@ -120,6 +120,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 CRONJOBS = [
     ('0 23 * * 0,2,4', 'killer.cron.scheduled'),
